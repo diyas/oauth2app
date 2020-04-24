7432ecd47e8e97f8cf780433225698828fe5fcfb
Diyas <diazch4ndra@gmail.com>
2020-04-24 13:11:19 +0700

    Oauth2 Remark Role From DB

--- src/main/java/com/auth/app/service/UserService.java
+++ src/main/java/com/auth/app/service/UserService.java
@@ -3,7 +3,6 @@ package com.auth.app.service;
 import com.auth.app.repository.UserMobileRepo;
 import com.auth.app.user.model.UserAuth;
 import com.auth.app.user.model.UserMobile;
-import com.auth.app.user.model.UserRoles;
 import com.auth.app.user.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,9 +33,9 @@ public class UserService implements UserDetailsService {
     }
 
     private List<SimpleGrantedAuthority> getAuthority(Users user) {
-        List<SimpleGrantedAuthority> list = new ArrayList<>();
-        for (UserRoles t : user.getUserRoles())
-            list.add(new SimpleGrantedAuthority(t.getRole().toString()));
+//        List<SimpleGrantedAuthority> list = new ArrayList<>();
+//        for (UserRoles t : user.getUserRoles())
+//            list.add(new SimpleGrantedAuthority(t.getRole().toString()));
         return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
     }
 }
